cc6980477e3de1645abef7e653c328dfa52c1e71
jamesh625 <z3375216@zmail.unsw.edu.au>
2016-03-17 18:11:02 +1100

    Update testList.c
    Will segfault at line 48...

--- testList.c
+++ testList.c
@@ -14,15 +14,50 @@ int main(int argc, char *argv[])
     assert(validDLList(myList));
 
     // Our tests.
-    
+    printf("\n\nStarting our tests.\n\n");
     // First thing, create an empty list by freeing myList.
+    printf ("Freeing myList...\n");
     free(myList);
     
     // Now reinitialise myList.
+    printf ("Reinitialising myList...\n");
     myList = newDLList();
-    // Check that list is really empty.
-    assert (DLListIsEmpty(myList));
     
+    // State (1): Empty list.
+    // Check that list is really empty and valid.
+    printf ("STATE (1): Empty List.\n");
+    printf ("Check myList empty and valid...\n");
+    assert (DLListIsEmpty(myList));
+    assert (validDLList(myList));
+    
+    // Now for the actual tests.
+    // Show list. Should be blank.
+    printf ("Showing myList...\n");
+    showDLList(stdout,myList);
+    
+    // Testing DLListBefore() (prepending).
+    printf ("Using DLListBefore()...\n");
+    DLListBefore(myList, "Line 1");
+    
+    // Show the list.
+    printf ("Showing myList...\n");
+    showDLList(stdout, myList);
+    
+    // Check not empty.
+    printf ("Check myList not empty...\n");
+    assert (!DLListIsEmpty(myList));
+    
+    // See what's at current and first and last.
+    printf ("Current line:   %s\n", DLListCurrent(myList));
+    DLListMoveTo(myList, 1);
+    //printf ("  First line:   %s\n", DLListCurrent(myList));
+    DLListMoveTo(myList, LAST_LINE);
+    //printf ("   Last line:   %s\n", DLListCurrent);
+    
+    
+    // Finally, check if list is still sane/valid.
+    assert (validDLList(myList));
     
     return EXIT_SUCCESS;
+
 }
